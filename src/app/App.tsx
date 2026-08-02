bb5b6a8d24a57bad4f73cad4e8a3d793a8bc0bde
trapintrovert <mcneo062@gmail.com>
2026-08-02 15:58:17 +0100

    Configure GitHub Pages deployment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from '../pages/home/HomePage';
 import { ManifestoPage } from '../pages/manifesto/ManifestoPage';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/manifesto" element={<ManifestoPage />} />
         <Route path="/manifest" element={<Navigate to="/manifesto" replace />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
